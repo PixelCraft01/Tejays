5fb20d7c5fc2ed9665b9ca51df2a48493c55edcc
Nilesh Prajapati <prajapatnilesh001@gmail.com>
2026-08-29 11:21:55 +0530

    Update website

--- src/pages/corporate/ManagedServices.jsx
+++ src/pages/corporate/ManagedServices.jsx
@@ -79,12 +79,11 @@ export default function ManagedServices() {
                 {/* =====================================================
                     HERO
                 ====================================================== */}
-
                 <section
                     className="
                         relative
                         overflow-hidden
-                        bg-red-50
+                        bg-red-100
                         py-24
                         sm:py-28
                         lg:py-36
@@ -105,6 +104,18 @@ export default function ManagedServices() {
                             bg-[#e3292f]/10
                             blur-[120px]
                         "
+                    />
+                    {/* Grid */}
+
+                    <div
+                        className="
+        pointer-events-none
+        absolute
+        inset-0
+        opacity-[0.04]
+        [background-image:linear-gradient(rgba(127,29,29,.8)_1px,transparent_1px),linear-gradient(90deg,rgba(127,29,29,.8)_1px,transparent_1px)]
+        [background-size:50px_50px]
+    "
                     />
 
                     <div
